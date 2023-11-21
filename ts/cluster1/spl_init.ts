33b2da31e8d3f971b3a94621e20514e7b7716b23
mattdegods <matthewcpfeifer@gmail.com>
2023-11-21 10:29:31 -0500

    Cohort class 1

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,19 +1,33 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { createMint } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
+import bs58 from "bs58";
+
+// base58 encoded private key
+const base58PrivateKey = wallet;
+// decode the base58 private key to a buffer
+const buffer = bs58.decode(base58PrivateKey);
+// convert the buffer to a Uint8Array
+const uint8ArrayPrivateKey = new Uint8Array(buffer);
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(uint8ArrayPrivateKey));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6
+    );
+    console.log(`Mint address: ${mint.toBase58()}`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,9 +1,17 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
+import bs58 from "bs58";
+
+// base58 encoded private key
+const base58PrivateKey = wallet;
+// decode the base58 private key to a buffer
+const buffer = bs58.decode(base58PrivateKey);
+// convert the buffer to a Uint8Array
+const uint8ArrayPrivateKey = new Uint8Array(buffer);
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(uint8ArrayPrivateKey));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
@@ -12,18 +20,31 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("5EktT9JZYhZpJ8RKV9ecEoJf9ZHxE3jShmRuXt9mawCf");
 
 (async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+  try {
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+      undefined,
+      commitment
+    );
+    console.log(`Your ata is: ${ata.address.toBase58()}`);
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+    // Mint to ATA
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      100n * token_decimals
+    );
+    console.log(`Your mint txid: ${mintTx}`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
